81acb96a911442f7416da7c8209c28c4d5ce4ce5
Lukas@influcom.fr <Lukas@influcom.fr>
2020-12-31 07:10:35 +0100

    last commit

--- racingjs/exercices/script/ex_01.js
+++ racingjs/exercices/script/ex_01.js
@@ -1,7 +1,5 @@
 var hello = function(){
-document.body.children[0].children[0].children[2].children[0].innerText = "Hello World";//Récupère tous les enfants du body jusqu'a la div de index 0.
+document.body.children[0].children[0].children[2].children[0].innerText = "Hello World";
 };
 hello();
-//document = objet qui contient des fonctions.
-//"."Liste les fonction à utilisées.
-//innerText est une propriété représentant le contenu textuel « visuellement rendu ».
+console.log(hello());
